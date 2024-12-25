d4bb949f99d23fd06bbf95cafca4d3880d99d0f9
Yash SD <yashgaikwadsmartdata@gmail.com>
2024-12-25 17:19:09 +0530

    Initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/sevices/JournalEntryService.java
@@ -0,0 +1,43 @@
+package net.engineeringdigest.journalApp.sevices;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
+    }
+
+//    public JournalEntry updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry myEntry){
+//        myEntry.setDate(LocalDateTime.now());
+//        journalEntryService.saveEntry(myEntry);
+//        return myEntry;
+//    }
+
+}
